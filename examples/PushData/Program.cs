ef6b9863b464a347f883aa99e08677c61716d0fc
Boris Ovčjak <boris.ovcjak@gmail.com>
2024-09-18 10:40:47 +0200

    Changed to async, formatting

--- examples/PushData/Program.cs
+++ examples/PushData/Program.cs
@@ -3,19 +3,22 @@ using Databox.Api;
 using Databox.Client;
 using Databox.Model;
 
-namespace Example {
-  public class Example {
-    public static void Main () {
+namespace Example
+{
+  public class Example
+  {
+    public static async Task Main(string[] args)
+    {
 
-      Configuration config = new Configuration ();
+      Configuration config = new Configuration();
       config.BasePath = "https://push.databox.com";
       config.Username = "<Your_Databox_API_Token>";
       config.DefaultHeaders.Add("Accept", "application/vnd.databox.v2+json");
 
 
-      HttpClient httpClient = new HttpClient ();
-      HttpClientHandler httpClientHandler = new HttpClientHandler ();
-      var apiInstance = new DefaultApi (httpClient, config, httpClientHandler);
+      HttpClient httpClient = new HttpClient();
+      HttpClientHandler httpClientHandler = new HttpClientHandler();
+      var apiInstance = new DefaultApi(httpClient, config, httpClientHandler);
       var dataPostRequest = new List<PushData>() {
         new PushData() {
           Key = "<Metric_name>",
@@ -31,10 +34,13 @@ namespace Example {
         }
       };
 
-      try {
-        var response = apiInstance.DataPostWithHttpInfo(dataPostRequest);
+      try
+      {
+        var response = await apiInstance.DataPostWithHttpInfoAsync(dataPostRequest);
         Console.WriteLine(response.Data.ToString());
-      } catch (ApiException e) {
+      }
+      catch (ApiException e)
+      {
         Console.WriteLine("Exception when calling DefaultApi.DataPostWithHttpInfo: " + e.Message);
         Console.WriteLine("Status Code: " + e.ErrorCode);
         Console.WriteLine(e.StackTrace);
